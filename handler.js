4ac3f30e41d75551a3e65908aacc8914423ad77a
PalakSaysHub <PalakN143@outlook.com>
2024-12-12 10:25:16 +0530

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,17 @@
+const status = "AUTOMATIC";
+const botName = "🌇 MULTI MUSIX";
+const rtxText = "MUSIFY";
+const version = "Latest@ v1.2";
+const startTime = Date.now();
+
+function printWatermark() {
+  const uptimeInSeconds = ((Date.now() - startTime) / 1000).toFixed(2);
+
+  console.log('\x1b[1m\x1b[36m╔════════════════════════════════════════════╗');
+  console.log('\x1b[1m\x1b[36m║║');
+  console.log(`\x1b[1m\x1b[36m            ${botName}     `);
+  console.log(`\x1b[1m\x1b[36m            👑 Authorization : ${status}    `);
+  console.log(`\x1b[1m\x1b[36m            💡 Version: ${version}`);
+  console.log(`\x1b[1m\x1b[36m            📅 Uptime: ${uptimeInSeconds}s`);
+  console.log(`\x1b[1m\x1b[36m            🚀 Powered by ${musifyText}`);
+  console.log('\x1b[1m\x1b[36m║                                            ║');
